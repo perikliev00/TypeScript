a0c9e682904bec702c624b87db2ecd9151782a5c
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-04-19 16:37:07 +0300

    Add files via upload

--- 22/22.ts
+++ 22/22.ts
@@ -51,3 +51,58 @@ class Rectangle implements Shape {
         return this.width * this.height;
     }
 }
+class Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    constructor(firstName: string, lastName: string, age:number) {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+const john = new Person("John", "Doe", 30);
+console.log(john.getFullName());
+class Account {
+    private balance: number;
+    constructor(initialBalance: number) {
+        this.balance=initialBalance;
+    }
+    deposit(amount: number): void {
+        this.balance+= amount;
+    }
+    withdraw(amount: number): void {
+        if(amount<=this.balance) {
+            this.balance-= amount;
+        } else {
+            console.log("Insifficiant balance");
+        }
+    }
+    getBalance(): number {
+        return this.balance;
+    }
+}
+const acc= new Account(1000);
+acc.deposit(500);
+class Temperature {
+    private _celsius: number;
+    constructor(celsius: number) {
+        this._celsius=celsius;
+    }
+    get celsius(): number {
+        return this._celsius;
+    }
+    set celsius(value: number) {
+        if(value< -273.15) {
+            throw new Error("Temperature cannot be below -273.15 C");
+        }
+        this._celsius=value;
+    }
+}
+const temp=new Temperature(25);
+console.log(temp.celsius);
+temp.celsius=30;
+console.log(temp.celsius);
