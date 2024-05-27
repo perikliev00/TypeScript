3df504893627e1352209a00f9981419dccee0628
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-05-28 00:30:36 +0300

    Add files via upload

---
+++ demo8/main.js
@@ -0,0 +1,34 @@
+var echo = function (arg) { return arg; };
+console.log(echo('alex'));
+var isObj = function (arg) {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+};
+console.log(isObj(true));
+console.log(isObj("John"));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
+console.log(isObj(null));
+///////////////////////
+var isTrue = function (arg) {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg: arg, is: false };
+    }
+    return { arg: arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({})); // modified
+console.log(isTrue({ name: 'Dave' }));
+console.log(isTrue([])); // modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
