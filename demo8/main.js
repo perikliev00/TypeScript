aee2b20e1e96326eb65e6dddea42bdcfc2cd1983
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-05-29 00:34:18 +0300

    Add files via upload

--- demo8/main.js
+++ demo8/main.js
@@ -32,3 +32,91 @@ console.log(isTrue([])); // modified
 console.log(isTrue([1, 2, 3]));
 console.log(isTrue(NaN));
 console.log(isTrue(-0));
+var chekBoolValue = function (arg) {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+var processUser = function (user) {
+    return user;
+};
+console.log(processUser({ id: 1, name: 'Dave' }));
+var getUsersProperty = function (users, key) {
+    return users.map(function (user) { return user[key]; });
+};
+var usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+];
+console.log(getUsersProperty(usersArray, "email"));
+console.log(getUsersProperty(usersArray, "username"));
+var StateObject = /** @class */ (function () {
+    function StateObject(value) {
+        this.data = value;
+    }
+    Object.defineProperty(StateObject.prototype, "state", {
+        get: function () {
+            return this.data;
+        },
+        set: function (value) {
+            this.data = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return StateObject;
+}());
+var store = new StateObject("John");
+console.log(store.state);
+store.state = "Dave";
+var myState = new StateObject([15]);
+myState.state = ['Dave', 42, true];
+console.log(myState.state);
